4b33ed98c7b207957fb008aca8dad03a6be51c58
whitewiz13 <sandeepthakur130197@gmail.com>
2023-03-13 16:55:30 +0530

    [Setup] Config settings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  output: 'export',
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+
 }
 
 module.exports = nextConfig
