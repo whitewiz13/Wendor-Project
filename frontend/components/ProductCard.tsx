a42bdeeace83ef7b0026bc3290cd4e36fb276cc1
whitewiz13 <sandeepthakur130197@gmail.com>
2023-03-14 23:16:37 +0530

    [Added] Product card image

--- frontend/components/ProductCard.tsx
+++ frontend/components/ProductCard.tsx
@@ -10,7 +10,9 @@ const ProductCard = ({ product, setSnackbarData }: any) => {
     }
     return (
         <div className="flex-auto max-w-sm rounded overflow-hidden border shadow-lg border-gray-400 m-5 mt-12 py-4">
-            {/* <img className="w-full" src={product.imageUrl} alt={product.name} /> */}
+            <div className="flex justify-center">
+                <img className="w-half w-20" src={product.image ? product.image : 'https://static.tildacdn.com/tild6563-6565-4335-b936-363465653063/logo.png'} alt={product.name} />
+            </div>
             <div className="px-6 py-4">
                 <div className="flex justify-between">
                     <div className="font-bold text-xl mb-2 text-gray-800">{product.name}</div>
