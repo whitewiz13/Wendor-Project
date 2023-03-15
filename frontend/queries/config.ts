00b9dfac7b5d4d3ab83868bb8e5b5de0f247417e
whitewiz13 <sandeepthakur130197@gmail.com>
2023-03-15 12:52:52 +0530

    [Change] Base url config

--- frontend/queries/config.ts
+++ frontend/queries/config.ts
@@ -1,3 +1,4 @@
 export const config = {
-    baseUrl: "http://192.168.29.194:4000/api/v1"
+    //baseUrl: "http://192.168.29.194:4000/api/v1"
+    baseUrl: "https://wendor-project-production.up.railway.app/api/v1"
 }
