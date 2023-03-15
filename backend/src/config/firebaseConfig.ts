4b5b4954f01939fcd05ab509373979dc9e1a8c24
whitewiz13 <sandeepthakur130197@gmail.com>
2023-03-15 15:30:15 +0530

    [Fix] Firebase keys

--- backend/src/config/firebaseConfig.ts
+++ backend/src/config/firebaseConfig.ts
@@ -1,17 +1,16 @@
 import * as admin from 'firebase-admin';
-import { serviceAccount } from './serviceAccount';
 
 const firebase_params = {
-    type: serviceAccount.type,
-    projectId: serviceAccount.project_id,
-    privateKeyId: serviceAccount.private_key_id,
-    privateKey: serviceAccount.private_key,
-    clientEmail: serviceAccount.client_email,
-    clientId: serviceAccount.client_id,
-    authUri: serviceAccount.auth_uri,
-    tokenUri: serviceAccount.token_uri,
-    authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-    clientC509CertUrl: serviceAccount.client_x509_cert_url
+    type: process.env.FIREBASE_TYPE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKeyId: process.env.FIREBASE_PRIVATE_KEY_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientId: process.env.FIREBASE_CLIENT_ID,
+    authUri: process.env.FIREBASE_AUTH_URI,
+    tokenUri: process.env.FIREBASE_TOKEN_URI,
+    authProviderX509CertUrl: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    clientC509CertUrl: process.env.FIREBASE_CLIENT_X509_CERT_URL
 }
 
 const firebaseApp = admin.initializeApp({
